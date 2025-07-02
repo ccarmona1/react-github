61e4dd4c11986d6ba8db23c043aaf8aa96149fa9
Crisman Carmona <ccarmona@endpointclosing.com>
2025-07-01 23:22:37 -0500

    Adding test coverage and GitHub actions

--- jest.config.js
+++ jest.config.js
@@ -26,4 +26,21 @@ export default {
     "!src/vite-env.d.ts",
     "!src/**/*.d.ts",
   ],
+  coverageDirectory: "coverage",
+  coverageReporters: [
+    "text",
+    "text-summary",
+    "lcov",
+    "html",
+    "json",
+    "json-summary",
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
 };
